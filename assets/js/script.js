c91a1c544badcef9898c12e42abb9eaf14b93b23
kylelawrencedev <kylelawrence19@gmail.com>
2021-03-23 11:34:24 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+/*-------------------------------------https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp*/
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+}
